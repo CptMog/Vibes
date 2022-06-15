d39b4743b303e9b5cb679c68723d703979b4b668
CptMog <djawad.mohamed8@gmail.com>
2022-06-15 23:32:17 +0200

    update the interfaces

--- src/components/Post.class.js
+++ src/components/Post.class.js
@@ -1,4 +1,11 @@
 import "./../styles/Post.css";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faEllipsisH } from '@fortawesome/free-solid-svg-icons';
+import { faHeart } from '@fortawesome/free-regular-svg-icons';
+import { faComment } from '@fortawesome/free-regular-svg-icons';
+import { faPaperPlane } from '@fortawesome/free-regular-svg-icons';
+import { faBookmark } from '@fortawesome/free-regular-svg-icons';
+
 function Post(props){
     return(
         <div className="container-post">
@@ -7,11 +14,24 @@ function Post(props){
                     <div className="post-profile-bulle">
 
                     </div>
-                    <span>{props.name}</span>
                 </div>
+                <div className="post-profile-infos">
+                    <span>{props.name}</span>
+                    <FontAwesomeIcon icon={faEllipsisH} id="post-settings"/>
+                </div>  
             </div>
             <div className="post-content">
-                <img src={props.img} alt="insta girl" />
+                <img src={props.img} alt="insta photo" />
+            </div>
+            <div className="post-profile-socials">
+                <div>
+                    <FontAwesomeIcon icon={faHeart} />
+                    <FontAwesomeIcon icon={faComment} />
+                    <FontAwesomeIcon icon={faPaperPlane} />
+                </div>
+                <div>
+                <FontAwesomeIcon icon={faBookmark} />
+                </div>
             </div>
         </div>
     )
